fb0f0442b95aee1548005fad8230749634bc4ff2
Wijekoon Mudiyanselage Shanka Primal Somasiri <soma48@c43176m.local>
2017-11-02 08:18:20 +1300

    Initial Commit

---
+++ ssomlk_project_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ssomlk_project_1
+//
+//  Created by Wijekoon Mudiyanselage Shanka Primal Somasiri on 2/11/17.
+//  Copyright © 2017 Wijekoon Mudiyanselage Shanka Primal Somasiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
